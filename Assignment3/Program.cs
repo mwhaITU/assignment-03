e7fbb6dd624692f0fe5023d3b33864f6e358ba57
eikbo <eikb@itu.dk>
2022-09-26 15:22:07 +0200

    Connects to local PostGres session instead of docker container

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -6,8 +6,17 @@ KanbanContext context = factory.CreateDbContext(args);
 var facade = context.Database;
 //facade.OpenConnection();
 
-var connString = "Host=localhost,41953;Username=postgres;Password=eikb0009;Database=postgres";
+var connString = "Host=localhost,41953;Username=postgres;Password=eikb0009;Database=homeworkOne";
 
 
 await using var conn = new NpgsqlConnection(connString);
 await conn.OpenAsync();
+await using var cmd = new NpgsqlCommand("Select id from person", conn);
+await using var reader = await cmd.ExecuteReaderAsync();
+
+while (await reader.ReadAsync())
+{
+    Console.WriteLine(reader.GetInt64(0));
+}
+
+
