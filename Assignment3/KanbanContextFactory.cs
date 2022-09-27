0b0a88cbc4e3ab7209987ae9da4ea76966ce046b
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2022-09-27 13:10:54 +0100

    This Work1!!!!!!!

--- Assignment3/KanbanContextFactory.cs
+++ Assignment3/KanbanContextFactory.cs
@@ -10,7 +10,7 @@ internal class KanbanContextFactory : IDesignTimeDbContextFactory<KanbanContext>
     public KanbanContext CreateDbContext(string[] args)
     {
         var configuration = new ConfigurationBuilder().Build();
-        var connectionString = "Server=localhost,41953;User Id=postgres;Password=eikb0009;Database=postgres";
+        //var connectionString = "Server=localhost,41953;User Id=postgres;Password=eikb0009;Database=postgres";
 
         var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>();
         //optionsBuilder.UseNpgsql(connectionString);
